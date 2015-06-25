0e30dce89d790c5e2cdfddf9a8febcfc3578887a
Emily <t-aadrian@traveloka.com>
2015-06-25 11:36:45 +0700

    Perbaikan  cara membaca data, namun masih akan disempurnakan

--- coal.js
+++ coal.js
@@ -1,10 +1,18 @@
+var input = ["Adaro Indonesia, PT","Allied Indo Coal, PT","Antang Gunung Meratus, PT","Arutmin Indonesia, PT"];
+
 function load() {
 	var mycoal = JSON.parse(coal_export);
 	
 	var text = "";
 	var i;
-	for (i = 0; i < mycoal.length; i++) { 
-		text += mycoal[i].nama_entitas + "<br>";
+	for (i = 0; i < mycoal.length; i++) {
+		var j; 
+		for (j = 0; j < input.length; j++ ){
+			if (mycoal[i].nama_entitas == input[j]){
+				text += mycoal[i].nama_entitas + "<br>";					
+			}
+		}
+
 	}	
 	document.getElementById("demo").innerHTML = text;
 
